b2514eb30c196d11145b8507fb78505068adb3cb
Jesualdo Cristovao <46726672+jesualdocc@users.noreply.github.com>
2020-12-22 19:50:35 -0700

    added files to git ignore

--- Backend/config.py
+++ Backend/config.py
@@ -4,13 +4,10 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    #DB CONFIG
+    #From Docker
     sql_server = "localhost"
-    sql_user = "root"
-    sql_password = "Jesualdo2020"
-    sql_db = "coviddb"
 
-    #PASSWORD HASHING
-    
-
+    sql_user = os.environ['DB_REMOTE_ROOT_NAME'] if 'DB_REMOTE_ROOT_NAME' in os.environ else 'Jesualdo'
+    sql_password = os.environ['DB_REMOTE_ROOT_PASS'] if 'DB_REMOTE_ROOT_PASS' in os.environ else 'Jesualdo2020'
+    sql_db = os.environ['DB_NAME'] if 'DB_NAME' in os.environ else 'coviddb'
 
